d30f5db0e8e913fe384f1d9250f21fed8ca90669
conceptblend <the.concept.blend@gmail.com>
2020-11-15 18:05:32 -0700

    refactored to a class and added layers

--- index.js
+++ index.js
@@ -1,139 +1,300 @@
 let data = {
-    config: {
-      min: 0,
-      max: 4,
+  range: {
+    min: 0,
+    max: 4,
+  },
+  meta: {
+    title: 'UX Discipline Profile',
+    dimensions: 'IxD,R:G,IA,R:E,u7y,a11y,UI,UxW,CS,AX,FED'.split(','),
+  },
+  profiles: [
+    {
+      name: 'Interaction Designer',
+      values: [
+        {
+          label: 'IxD',
+          value: 4,
+        },
+        {
+          label: 'R:E',
+          value: 2,
+        },
+        {
+          label: 'UI',
+          value: 2,
+        },
+        {
+          label: 'AX',
+          value: 4,
+        },
+        {
+          label: 'IA',
+          value: 1,
+        },
+        {
+          label: 'CS',
+          value: 0,
+        },
+        {
+          label: 'UxW',
+          value: 2,
+        },
+        {
+          label: 'R:G',
+          value: 1,
+        },
+        {
+          label: 'u7y',
+          value: 3,
+        },
+
+        {
+          label: 'a11y',
+          value: 0,
+        },
+      ],
     },
-    meta: {
-      title: "UX Discipline Profile"
+    {
+      name: 'Brian',
+      values: [
+        {
+          label: 'IxD',
+          value: 1,
+        },
+        {
+          label: 'R:E',
+          value: 1,
+        },
+        {
+          label: 'UI',
+          value: 4,
+        },
+        {
+          label: 'AX',
+          value: 1,
+        },
+        {
+          label: 'IA',
+          value: 1,
+        },
+        {
+          label: 'CS',
+          value: 1,
+        },
+        {
+          label: 'UxW',
+          value: 1,
+        },
+        {
+          label: 'R:G',
+          value: 1,
+        },
+        {
+          label: 'u7y',
+          value: 1,
+        },
+
+        {
+          label: 'a11y',
+          value: 1,
+        },
+      ],
     },
-    values: [{
-        label: "IxD",
-        value: 3
-      },
-      {
-        label: "UI",
-        value: 1
-      },
-      {
-        label: "AX",
-        value: 2
-      },
-      {
-        label: "IA",
-        value: 2
-      },
-      {
-        label: "CS",
-        value: 2
-      },
-      {
-        label: "UxW",
-        value: 2
-      },
-      {
-        label: "R:G",
-        value: 2
-      },
-      {
-        label: "R:E",
-        value: 3
-      },
-      {
-        label: "u7y",
-        value: 3
-      },
-      {
-        label: "a11y",
-        value: 3
-      },
-    ]
-  };
-  let center = {
-    x: null,
-    y: null
-  }
-  let g_textSize = 14;
-  
-  function setup() {
-    createCanvas(400, 400);
-    center.x = width / 2;
-    center.y = height / 2;
-    angleMode(DEGREES);
-    textSize(g_textSize);
-    strokeCap(SQUARE);
-    strokeJoin(ROUND);
-    noLoop();
-  }
-  
-  function draw() {
-    background(255);
-  
-    let n = data.values.length;
-    let deg = 360 / n;
-    let chartPoints = [];
-    for (i = 0; i < n; i++) {
-      chartPoints.push(calcChartPointForDimension(40, data.values[i].value, i, n));
-    }
-    console.log(chartPoints);
-  
-    // Draw lines out from the center as the base
-    strokeWeight(3);
-    stroke(192);
-    for (i = 0; i < n; i++) {
-      var x = (40 * data.config.max) * cos(deg * i);
-      var y = (40 * data.config.max) * sin(deg * i);
-      line(center.x, center.y, center.x + x, center.y + y);
-    }
-  
+    {
+      name: 'Chris the Usability Specialist',
+      values: [
+        {
+          label: 'IxD',
+          value: 0,
+        },
+        {
+          label: 'R:E',
+          value: 3,
+        },
+        {
+          label: 'UI',
+          value: 1,
+        },
+        {
+          label: 'AX',
+          value: 3,
+        },
+        {
+          label: 'IA',
+          value: 2,
+        },
+        {
+          label: 'CS',
+          value: 1,
+        },
+        {
+          label: 'UxW',
+          value: 1,
+        },
+        {
+          label: 'R:G',
+          value: 2,
+        },
+        {
+          label: 'u7y',
+          value: 4,
+        },
+        {
+          label: 'a11y',
+          value: 3,
+        },
+      ],
+    },
+  ],
+};
+
+function setup() {
+  createCanvas(400, 400);
+  angleMode(DEGREES);
+  textSize(14);
+  strokeCap(SQUARE);
+  strokeJoin(ROUND);
+  noLoop();
+}
+
+function draw() {
+  background(255);
+
+  let pg = new ProfileGraph(
+    data.meta.dimensions,
+    { x: width / 2, y: height / 2 },
+    data.range
+  );
+
+  // Draw and label lines out from the center as the base
+
+  pg.drawDimensions();
+
+  for (var n = 0, len = data.profiles.length; n < len; n++) {
     // Describe the look of the polygon
     noStroke();
-    fill('rgba(0,255,0, .8)');
-    drawChartPolygon(chartPoints);
-  
-  
-    // Draw lines out from the center
-    stroke(0)
-    drawChartDimensionValues(chartPoints);
-  
-    noStroke();
-    fill(32);
-    for (i = 0; i < n; i++) {
-      var pos = getLabelPosition(data.values[i].label, deg * i);
-      text(data.values[i].label, pos.x, pos.y);
-    }
+    // stroke(255);
+    fill('rgba(0, 255, 128, .5)');
+    pg.drawChartPolygon(data.profiles[n].values);
+
   }
-  
-  function calcChartPointForDimension(magnitude, value, dimension_index, dimension_count) {
-    let deg = 360 / dimension_count;
-    return {
-        x: (magnitude * value) * cos(deg * dimension_index),
-        y: (magnitude * value) * sin(deg * dimension_index),
+
+  // Draw lines out from the center
+  for (var n = 0, len = data.profiles.length; n < len; n++) {
+    stroke(0);
+    strokeWeight(5);
+    pg.drawChartDimensionValues(data.profiles[n].values);
+  }
+}
+
+class ProfileGraph {
+  constructor(dimensions, center, range) {
+    this.dimensions = dimensions; // [ ]: string
+    this.angle = 360 / dimensions.length;
+    this.center = center; // { x: x', y: y' }
+    this.range = range; // { min: n, max: m }
+
+    this.getDimensionIndexFromLabel = (label) => this.dimensions.indexOf(label);
+  }
+
+  drawChartPolygon(_vals) {
+    // Setup a zero-out array for all of the dimensions and then
+    // fill the known values.
+    let vals = [];
+    for (var n = 0, len = this.dimensions.length; n < len; n++) {
+      vals.push({
+        label: this.dimensions[n],
+        value: 0,
+      });
+    }
+
+    for (var n = 0, len = _vals.length; n < len; n++) {
+      var i = this.getDimensionIndexFromLabel(_vals[n].label);
+      if (i !== undefined) {
+        vals[i].value = _vals[n].value;
       }
-  }
-  
-  function drawChartDimensionValues(vals) {
-    for (i = 0, len = vals.length; i < len; i++) {
-      line(center.x, center.y, center.x + vals[i].x, center.y + vals[i].y);
     }
-  }
-  
-  function drawChartPolygon(vals) {
-    let len = vals.length;
+    // SORT
+    // let len = _vals.length;
+    // let self = this;
+    // // Sort the vals so they're in the right order
+    // let vals = _vals.sort(function (a, b) {
+    //   let a_i = self.getDimensionIndexFromLabel(a.label);
+    //   let b_i = self.getDimensionIndexFromLabel(b.label);
+    //   if (a_i < b_i) return -1;
+    //   if (a_i > b_i) return 1;
+    //   return 0;
+    // });
+
     // Draw the polygon
     beginShape();
-    for (i = 0; i < len; i++) {
-      vertex(center.x + vals[i].x, center.y + vals[i].y);
+    for (var i = 0, len = vals.length; i < len; i++) {
+      var pt = this.calcChartPointForDimension(
+        40,
+        vals[i].value,
+        vals[i].label
+      );
+      vertex(this.center.x + pt.x, this.center.y + pt.y);
     }
     endShape(CLOSE);
   }
-  
-  
-  function getLabelPosition(label, angle) {
-    // calcChartPointForDimension(45, data.config.max, i, n)
-    let ox = (45 * data.config.max) * cos(angle);
-    let oy = (45 * data.config.max) * sin(angle);
+
+  drawChartDimensionValues(vals) {
+    for (var i = 0, len = vals.length; i < len; i++) {
+      var pt = this.calcChartPointForDimension(
+        40,
+        vals[i].value,
+        vals[i].label
+      );
+
+      stroke(2);
+      line(
+        this.center.x,
+        this.center.y,
+        this.center.x + pt.x,
+        this.center.y + pt.y
+      );
+    }
+  }
+
+  calcChartPointForDimension(magnitude, value, label) {
+    let i = this.getDimensionIndexFromLabel(label);
+    let length = magnitude * value;
+    let theta = -90 + this.angle * i;
     return {
-      x: center.x + ox - (textWidth(label) / 2),
-      y: center.y + oy + (g_textSize / 2)
+      x: length * cos(theta),
+      y: length * sin(theta),
     };
   }
+  getLabelPosition(label) {
+    let pt = this.calcChartPointForDimension(45, this.range.max, label);
+    return {
+      x: pt.x - textWidth(label) / 2,
+      y: pt.y + textSize() / 2,
+    };
+  }
+
+  drawDimensions() {
+    let n = this.dimensions.length;
+    for (var i = 0; i < n; i++) {
+      var pt = this.calcChartPointForDimension(
+        40,
+        this.range.max,
+        this.dimensions[i]
+      );
+
+      strokeWeight(2);
+      stroke(192);
+      line(
+        this.center.x,
+        this.center.y,
+        this.center.x + pt.x,
+        this.center.y + pt.y
+      );
+      noStroke();
+      fill(32);
+      var pos = this.getLabelPosition(this.dimensions[i]);
+      text(this.dimensions[i], this.center.x + pos.x, this.center.y + pos.y);
+    }
+  }
+}
